6e17a6cfc8b85472016e7036f7bafc89b2f8759c
Chris Rose <chrisrose@Chriss-MacBook-Pro.local>
2016-11-10 22:00:05 +1100

    staticfiles-dir

--- pop_quiz/settings.py
+++ pop_quiz/settings.py
@@ -144,5 +144,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "static"),
+    os.path.join(BASE_DIR, "static"),
 ]
