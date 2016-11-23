4e9ae5a29aff9aae0c87a313bd5c5845d82b31f7
Chris Rose <chrisrose@Chriss-MacBook-Pro.local>
2016-11-23 18:00:10 +1100

    wss

--- static/polls/poll_client_interface.js
+++ static/polls/poll_client_interface.js
@@ -1,4 +1,4 @@
-socket = new WebSocket("ws://" + window.location.host + "?" + session_key);
+socket = new WebSocket("wss://" + window.location.host + "?" + session_key);
 
 var width = 420,
     barHeight = 50;
