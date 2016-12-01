7b4b10ec6eab423c1464e879c7161e37b98a35a9
Chris <chris.rose.one@gmail.com>
2016-12-01 18:12:43 +1100

    4 description states

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,8 @@ def poll(request):
 			if not question.id == request.session.get('loaded_question'):
 				request.session['loaded_question'] = question.id
 				request.session['has_voted'] = False
-			return render(request, 'polls/poll.html', {'question': question, 'choices': question.choices_as_json()})
+			return render(request, 'polls/poll.html', {
+				'question': question, 'choices': question.choices_as_json(), 'has_voted': request.session['has_voted']})
 	else:
 		return render(request, 'polls/poll.html', {
 			'error_message': "There isn't a poll running at the moment!"
