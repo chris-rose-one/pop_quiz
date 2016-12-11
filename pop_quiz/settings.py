ddf433cc52b4f7fee01de3aac87f9b7e1a959832
chris <chris.rose.one@gmail.com>
2016-12-11 14:24:42 +1100

    allowed_hosts change

--- pop_quiz/settings.py
+++ pop_quiz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['limitless-gorge-42955.herokuapp.com']
+ALLOWED_HOSTS = ['mysterious-mesa-93238.herokuapp.com']
 
 
 # Application definition
